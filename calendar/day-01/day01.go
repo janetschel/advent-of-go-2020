0664a361beab2a504905387b62a43ef8b94c5b84
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-01 07:32:44 +0100

    Reworked solutions for day 1

--- calendar/day-01/day01.go
+++ calendar/day-01/day01.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"advent-of-go-2020/utils"
+	"errors"
+	"fmt"
+	"log"
 )
 
 func main() {
 	inputSliceAsString := utils.ReadFile(1, "\n")
 	input := utils.ToIntSlice(inputSliceAsString)
 
+	solution, err := findPairToMakeSum(input, 2020)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(solution)
+}
+
+func findPairToMakeSum(input []int, sum int) (int, error) {
 	for i := 0; i < len(input); i++ {
 		for j := i + 1; j < len(input); j++ {
-			if input[i] + input[j] == 2020 {
-				println(input[i] * input[j])
-				return
+			if input[i] + input[j] == sum {
+				return input[i] * input[j], nil
 			}
 		}
 	}
-}
 
+	return 0, errors.New(fmt.Sprintf("No pair found to make sum of %d", sum))
+}
