68555b8e25cf988624bea6b17dfcc2b34ade6c61
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-01 08:05:09 +0100

    Fixed bug so that newline added by fetch utils does not affect input which is not sperated by a newline

--- utils/file.go
+++ utils/file.go
@@ -33,7 +33,22 @@ func ReadFile(day int, delimiter string) []string {
 	fileContent := string(file)
 	slicedContent := strings.Split(fileContent, delimiter)
 
-	return slicedContent[:len(slicedContent) - 1]
+	if delimiter == "\n" {
+		// fetch utils always adds a new line at the end of a file, which could lead to some problems when parsing it
+		// this is why the last row is just removed if the delimiter is a newline
+
+		return slicedContent[:len(slicedContent) - 1]
+	} else {
+		// if the delimiter is not a newline and we split on eg. a comma, the newline will be appended to the last
+		// element in the slice which then cannot be converted to an int.
+		// this is the reason the last element in the slice is modified (the last char is removed
+		// [which is the extra newline]) so it can be worked with
+
+		lastElement := slicedContent[len(slicedContent) - 1]
+		slicedContent[len(slicedContent) - 1] = lastElement[:len(lastElement) - 1]
+
+		return slicedContent
+	}
 }
 
 func createFile(day int, filePath string) {
