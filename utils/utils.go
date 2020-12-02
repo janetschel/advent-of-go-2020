1e336445c224b62c10e9715301a0104bfded1eea
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-02 09:29:02 +0100

    Added various helper functions in utils

--- utils/utils.go
+++ utils/utils.go
@@ -11,7 +11,7 @@ func ToIntSlice(slice []string) []int {
 		convertedString, err := strconv.Atoi(current)
 
 		if err != nil {
-			panic("Can't convert to int.. Please check the validity your input AND your specified delimiter")
+			panic(err)
 		}
 
 		sliceToReturn = append(sliceToReturn, convertedString)
@@ -22,9 +22,32 @@ func ToIntSlice(slice []string) []int {
 
 func ToInt(str string) int {
 	number, err := strconv.Atoi(str)
+
 	if err != nil {
 		panic(err)
 	}
 
 	return number
+}
+
+func Unpack(slice []string, vars... *string) {
+	for i, str := range slice {
+		*vars[i] = str
+	}
+}
+
+func Filter(slice []string, filter func(string) bool) []string {
+	retSlice := []string{}
+
+	for _, element := range slice {
+		if filter(element) {
+			retSlice = append(retSlice, element)
+		}
+	}
+
+	return retSlice
+}
+
+func Count(slice []string) int {
+	return len(slice)
 }
