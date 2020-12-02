1090f0c03236dd90ea2bc4cc29dc4172bd758eba
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-02 07:25:31 +0100

    Added utils method to convert to int more easily

--- utils/utils.go
+++ utils/utils.go
@@ -19,3 +19,12 @@ func ToIntSlice(slice []string) []int {
 
 	return sliceToReturn
 }
+
+func ToInt(str string) int {
+	number, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+
+	return number
+}
