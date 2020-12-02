2840692efbfd3adfc7435838ee63b26904fe0065
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-02 12:34:23 +0100

    Code golfing day 02

--- calendar/day-02/day02.go
+++ calendar/day-02/day02.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"advent-of-go-2020/utils"
-	"fmt"
 	"strings"
 )
 
 func main() {
 	input := utils.ReadFile(2, "\n")
 	pwds := utils.Filter(input, isPasswordValid)
-	num := utils.Count(pwds)
-
-	fmt.Printf("Number of valid passwords: %d", num)
+	println(utils.Count(pwds))
 }
 
 func isPasswordValid(currentElement string) bool {
-	parts := strings.Split(currentElement, " ")
-
 	var numbers, char, pwd, lo, hi string
-	utils.Unpack(parts, &numbers, &char, &pwd)
+	utils.Unpack(strings.Split(currentElement, " "), &numbers, &char, &pwd)
 	utils.Unpack(strings.Split(numbers, "-"), &lo, &hi)
 
 	numChars := strings.Count(pwd, char[:len(char) - 1])
--- calendar/day-02/day02_pt02.go
+++ calendar/day-02/day02_pt02.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"advent-of-go-2020/utils"
-	"fmt"
 	"strings"
 )
 
 func main() {
 	input := utils.ReadFile(2, "\n")
 	pwds := utils.Filter(input, isPasswordValidCharPos)
-	num := utils.Count(pwds)
-
-	fmt.Printf("Number of valid passwords: %d", num)
+	println(utils.Count(pwds))
 }
 
 func isPasswordValidCharPos(currentElement string) bool {
-	parts := strings.Split(currentElement, " ")
+	var numbers, char, pwd, lo, hi string
+	utils.Unpack(strings.Split(currentElement, " "), &numbers, &char, &pwd)
+	utils.Unpack(strings.Split(numbers, "-"), &lo, &hi)
 
-	var numbers, char, pwd, i, j string
-	utils.Unpack(parts, &numbers, &char, &pwd)
-	utils.Unpack(strings.Split(numbers, "-"), &i, &j)
+	i, errFirst := utils.CharAt(pwd, utils.ToInt(lo) - 1)
+	j, errSecond := utils.CharAt(pwd, utils.ToInt(hi) - 1)
 
-	char = char[:len(char) - 1]
-	first, errFirst := utils.CharAt(pwd, utils.ToInt(i) - 1)
-	second, errSecond := utils.CharAt(pwd, utils.ToInt(j) - 1)
-
-	return errFirst == nil && errSecond == nil && (first == char) != (second == char)
+	return errFirst == nil && errSecond == nil && (i == char[:len(char) - 1]) != (j == char[:len(char) - 1])
 }
