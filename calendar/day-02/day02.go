36b1d4c82892a1fd513c74847c34f73ea4ee9749
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-02 07:01:42 +0100

    Day 02 Part 1

--- calendar/day-02/day02.go
+++ calendar/day-02/day02.go
@@ -3,9 +3,43 @@ package main
 import (
 	"advent-of-go-2020/utils"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	inputSliceAsString := utils.ReadFile(2, "\n")
-	fmt.Println(inputSliceAsString)
+	input := utils.ReadFile(2, "\n")
+
+	numberOfKnownValidPasswords := 0
+
+	for _, currentElement := range input {
+		if isPasswordValid(currentElement) {
+			numberOfKnownValidPasswords++
+		}
+	}
+
+	fmt.Printf("Number of valid passwords: %d", numberOfKnownValidPasswords)
+}
+
+func isPasswordValid(currentElement string) bool {
+	partsOfLine := strings.Split(currentElement, ":")
+	numbers := strings.Split(partsOfLine[0], "-")
+
+	lowerBound, _ := strconv.Atoi(numbers[0])
+	upperBound, _ := strconv.Atoi(numbers[1][:len(numbers[1]) - 2])
+	charToSearch := numbers[1][len(numbers[1]) - 1:]
+	password := partsOfLine[1][1:]
+
+	currentNumberOfChar := 0
+	for _, char := range password {
+		if string(char) == charToSearch {
+			currentNumberOfChar++
+		}
+
+		if currentNumberOfChar > upperBound {
+			return false
+		}
+	}
+
+	return currentNumberOfChar >= lowerBound
 }
