d23fdcd34be0803df124eeb545e9a6e204f2ef14
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-02 10:07:24 +0100

    Accidentally removed cached files

---
+++ utils/request.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"advent-of-go-2020/secrets"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func makeRequest(day int) string {
+	url := fmt.Sprintf("https://adventofcode.com/2020/day/%v/input", day)
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Cookie", secrets.Session)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body)
+}
