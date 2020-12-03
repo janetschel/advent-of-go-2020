48df274c0e40dcb257461158a4d37496bfbb1aec
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-03 06:50:09 +0100

    Day 3 part 1 and 2

--- calendar/day-03/day03.go
+++ calendar/day-03/day03.go
@@ -2,14 +2,42 @@ package main
 
 import (
 	"advent-of-go-2020/utils/files"
+	"fmt"
 )
 
+
+
+// For this day this file contains the solution for part 1 and part 2, since they are almost identical
 func main() {
-	// Add correct day and delimiter for automatic input fetch
 	input := files.ReadFile(3, "\n")
-	println(input)
+
+	fmt.Printf("Solution part 1: %d\n", solvePart1(input))
+	fmt.Printf("Solution part 2: %d", solvePart2(input))
 }
 
-func solve() {
+func solvePart1(input []string) int {
+	return numTrees(input, 3, 1)
+}
 
+func solvePart2(input []string) int {
+	result := 1
+
+	for i := 1; i <= 7; i += 2 {
+		result *= numTrees(input, i, 1)
+	}
+
+	result *= numTrees(input, 1, 2)
+	return result
+}
+
+func numTrees(input []string, deltax int, deltay int) int {
+	trees := 0
+
+	for y, x := 0, 0; y < len(input); y, x = y + deltay, x + deltax {
+		if string(input[y][x%len(input[y])]) == "#" {
+			trees++
+		}
+	}
+
+	return trees
 }
--- go.mod
+++ go.mod
@@ -1,2 +1,3 @@
-module "advent-of-go-2020"
+module advent-of-go-2020
 
+go 1.14
