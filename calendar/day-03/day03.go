f0e611f2fecd18b408739335eac6527924f6c80e
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-03 08:25:23 +0100

    Split day03 into part 1 and 2 for better readbility (no changes on actual code)

--- calendar/day-03/day03.go
+++ calendar/day-03/day03.go
@@ -5,27 +5,9 @@ import (
 	"fmt"
 )
 
-// For this day this file contains the solution for part 1 and part 2, since they are almost identical
 func main() {
 	input := files.ReadFile(3, "\n")
-
-	fmt.Printf("Solution part 1: %d\n", solvePart1(input))
-	fmt.Printf("Solution part 2: %d", solvePart2(input))
-}
-
-func solvePart1(input []string) int {
-	return numTrees(input, 3, 1)
-}
-
-func solvePart2(input []string) int {
-	result := 1
-
-	for i := 1; i <= 7; i += 2 {
-		result *= numTrees(input, i, 1)
-	}
-
-	result *= numTrees(input, 1, 2)
-	return result
+	fmt.Printf("Solution part 1: %d\n", numTrees(input, 3, 1))
 }
 
 func numTrees(input []string, deltax int, deltay int) int {
--- calendar/day-03/day03_pt02.go
+++ calendar/day-03/day03_pt02.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"advent-of-go-2020/utils/files"
+	"fmt"
+)
+
 func main() {
-	$END$
+	input := files.ReadFile(3, "\n")
+	result := 1
+
+	// Function to solve included in part 1 (they are identical)
+	for i := 1; i <= 7; i += 2 {
+		result *= numTrees(input, i, 1)
+	}
+
+	result *= numTrees(input, 1, 2)
+
+	fmt.Printf("Solution part 2: %d", result)
 }
