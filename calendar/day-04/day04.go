0f987d610cfabf9fd8ab7e9685790a0a93f0c240
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-04 08:14:48 +0100

    Solved Part 1 and 2 of day 4

--- calendar/day-04/day04.go
+++ calendar/day-04/day04.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"advent-of-go-2020/utils/files"
+	"strings"
 )
 
 func main() {
-	input := files.ReadFile(0, "\n")
-
-	solve(input)
-
-	println(input)
+	input := files.ReadFile(4, "\n\n")
+	println(solve(input))
 }
 
-func solve(input []string) {
+func solve(input []string) int {
+	fields := []string {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	valid := 0
 
+	for _, element := range input {
+		for i, field := range fields {
+			if !strings.Contains(element, field) {
+				break
+			} else if i == 6 {
+				valid++
+			}
+		}
+	}
+
+	return valid
 }
