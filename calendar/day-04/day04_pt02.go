4c6e5a2cb3b91c727f78455117f03646fd9bd2c3
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-04 08:27:02 +0100

    Added comment for better understanding

--- calendar/day-04/day04_pt02.go
+++ calendar/day-04/day04_pt02.go
@@ -50,7 +50,7 @@ func validate(element string) bool {
 		return false
 	} else if (unit == "cm" && (hgt < 150 ||  hgt > 193)) || (unit == "in" && (hgt < 59 || hgt > 76)) {
 		return false
-	}else if matched, err := regexp.MatchString("#([0-9a-f]){6}", parts["hcl"]); !matched || err != nil {
+	} else if matched, err := regexp.MatchString("#([0-9a-f]){6}", parts["hcl"]); !matched || err != nil {
 		return false
 	} else if _, err := strconv.Atoi(parts["pid"]); err != nil || len(parts["pid"]) != 9 {
 		return false
