c6a0ccb90843c98705b93feae63893d3a9138dbd
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-06 06:49:18 +0100

    Solve day 1 part 1,2 and writeup

--- calendar/day-06/day06.go
+++ calendar/day-06/day06.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"advent-of-go-2020/utils/files"
+	"go/types"
+	"strings"
 )
 
 func main() {
-	input := files.ReadFile(6, "\n")
-	println(solve(input))
+	input := files.ReadFile(6, "\n\n")
+	println("Count answers part 1:", solve(input))
 }
 
 func solve(input []string) int {
+	count := 0
+	for _, curr := range input {
+		count += answers(curr)
+	}
 
-	return 1
+	return count
+}
+
+func answers(group string) int{
+	givenAnswers := make(map[string]types.Nil)
+	count := 0
+
+	for _, line := range strings.Split(group, "\n") {
+		for _, char := range line {
+			if _, present := givenAnswers[string(char)]; !present {
+				givenAnswers[string(char)] = types.Nil{} // make it present
+				count++
+			}
+		}
+	}
+
+	return count
 }
--- calendar/day-06/day06_pt02.go
+++ calendar/day-06/day06_pt02.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"advent-of-go-2020/utils/files"
+	"strings"
 )
 
 func main() {
-	input := files.ReadFile(6, "\n")
-	println(solve(input))
+	input := files.ReadFile(6, "\n\n")
+	println("Count answers part 2:", solvePart2(input))
+
 }
 
 func solvePart2(input []string) int {
+	count := 0
+	for _, curr := range input {
+		count += answersPart2(curr)
+	}
 
-	return 1
+	return count
+}
+
+func answersPart2(group string) int{
+	givenAnswers := make(map[string]int)
+	lines := strings.Split(group, "\n")
+
+	for _, line := range lines {
+		for _, char := range line {
+			givenAnswers[string(char)] += 1
+		}
+	}
+
+	count := 0
+	for value := range givenAnswers {
+		if ans, present := givenAnswers[value]; present && ans == len(lines) {
+			count += 1 // everyone answered yes
+		}
+	}
+
+	return count
 }
