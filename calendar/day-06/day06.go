cd3530a0828a73d19d7b78b9b4895a90c07cbebc
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-06 07:01:39 +0100

    Solved typo in prev commit message, solved day --> 6 <--

---
+++ calendar/day-06/day06.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"advent-of-go-2020/utils/files"
+	"go/types"
+	"strings"
+)
+
+func main() {
+	input := files.ReadFile(6, "\n\n")
+	println("Count answers part 1:", solve(input))
+}
+
+func solve(input []string) int {
+	count := 0
+	for _, curr := range input {
+		count += answers(curr)
+	}
+
+	return count
+}
+
+func answers(group string) int{
+	givenAnswers := make(map[rune]types.Nil)
+	count := 0
+
+	for _, line := range strings.Split(group, "\n") {
+		for _, char := range line {
+			if _, present := givenAnswers[char]; !present {
+				givenAnswers[char] = types.Nil{} // make it present
+				count++
+			}
+		}
+	}
+
+	return count
+}
