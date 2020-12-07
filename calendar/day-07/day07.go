772f80d96151458f5ef8ec95f9e6c42bec096ef5
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-07 05:51:42 +0100

    Setup day 7

---
+++ calendar/day-07/day07.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"advent-of-go-2020/utils/files"
+)
+
+func main() {
+	input := files.ReadFile(7, "\n")
+	println(solve(input))
+}
+
+func solve(input []string) int {
+
+	return 1
+}
---
+++ calendar/day-07/day07_pt02.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"advent-of-go-2020/utils/files"
+)
+
+func main() {
+	input := files.ReadFile(7, "\n")
+	println(solvePart2(input))
+}
+
+func solvePart2(input []string) int {
+
+	return 1
+}
