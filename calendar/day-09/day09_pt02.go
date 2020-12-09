bae398534468aab83c270f89342204c641135768
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-09 05:41:03 +0100

    Setup day 9

---
+++ calendar/day-09/day09_pt02.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"advent-of-go-2020/utils/files"
+)
+
+func main() {
+	input := files.ReadFile(9, "\n")
+	println(solvePart2(input))
+}
+
+func solvePart2(input []string) int {
+    result := 0
+
+
+
+    return result
+}
