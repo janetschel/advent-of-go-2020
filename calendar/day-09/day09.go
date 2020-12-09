ad9e02f6fb67a531d360518930ba41050c972e30
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-09 06:58:09 +0100

    Solution day 9 part 1 and 2

--- calendar/day-09/day09.go
+++ calendar/day-09/day09.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"advent-of-go-2020/utils/conv"
 	"advent-of-go-2020/utils/files"
+	"errors"
 )
 
 func main() {
 	input := files.ReadFile(9, "\n")
-	println(solvePart1(input))
+	result, err := solvePart1(conv.ToIntSlice(input))
+
+	if err != nil {
+		panic(err)
+	}
+
+	println("First number with no pair of sum in preamble:", result)
 }
 
-func solvePart1(input []string) int {
-    result := 0
+func solvePart1(input []int) (int, error) {
+	preamble := 25
 
+	for i := preamble; i < len(input); i++ {
+		valid := false
 
+		for j := i - preamble; j < i; j++ {
+			for k := j + 1; k < i; k++ {
+				if input[j] + input[k] == input[i] {
+					valid = true
+				}
+			}
+		}
 
-    return result
+		if !valid {
+			return input[i], nil
+		}
+	}
+
+	return 0, errors.New("no number without sum in preamble found")
 }
