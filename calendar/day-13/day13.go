100869254f0cf53297d9e8f43f7cf1fb3d7a000f
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-13 07:01:31 +0100

    Day 13 NOT REFACTORED YET

--- calendar/day-13/day13.go
+++ calendar/day-13/day13.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"advent-of-go-2020/utils/conv"
 	"advent-of-go-2020/utils/files"
+	"advent-of-go-2020/utils/maths"
+	"strings"
 )
 
 func main() {
@@ -10,9 +13,36 @@ func main() {
 }
 
 func solvePart1(input []string) int {
-    result := 0
+    earliestDep := conv.ToInt(input[0])
+    depTime := parseLine(input[1])
 
+    earliest := 100000000000
+    id := 0
+    for _, element := range depTime {
+    	curr := 0
+    	for curr <= earliestDep {
+    		curr += element
+		}
 
+		if curr < earliest {
+			id = element
+			earliest = curr
+		}
+	}
 
-    return result
+	minutes := earliestDep - earliest
+    return maths.Abs(minutes) * id
+}
+
+func parseLine(input string) []int {
+	split := strings.Split(input, ",")
+	result := make([]int, 0)
+
+	for _, element := range split {
+		if element != "x" {
+			result = append(result, conv.ToInt(element))
+		}
+	}
+
+	return result
 }
--- calendar/day-13/day13_pt02.go
+++ calendar/day-13/day13_pt02.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"advent-of-go-2020/utils/conv"
 	"advent-of-go-2020/utils/files"
+	"strings"
 )
 
 func main() {
@@ -10,9 +12,44 @@ func main() {
 }
 
 func solvePart2(input []string) int {
-    result := 0
+	depTime, minutesApart := parseLine2(input[1])
+	sum := depTime[0] + minutesApart[0]
+	dep := depTime[0]
 
+	for j := 1; j < len(depTime); j++ {
+		println(minutesApart[j], depTime[j])
+		for (sum + minutesApart[j]) % depTime[j] != 0 {
+			sum += dep
+		}
+		dep = (dep * depTime[j]) / gcd(dep, depTime[j])
+	}
 
+	return sum
+}
 
-    return result
+func parseLine2(input string) ([]int, []int) {
+	split := strings.Split(input, ",")
+	result := make([]int, 0)
+	times := make([]int, 0)
+
+	for i, element := range split {
+		if element != "x" {
+			result = append(result, conv.ToInt(element))
+			times = append(times, i)
+		}
+	}
+
+	return result, times
+}
+
+func gcd(first int, second int) int {
+	var div int
+
+	for i := 1; i <= first && i <= second; i++ {
+		if first % i==0 && second % i==0 {
+			div = i
+		}
+	}
+
+	return div
 }
