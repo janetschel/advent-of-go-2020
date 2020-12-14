1a4351a4a6cf9e29cf34ca5496dcddab4a5c76fc
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-14 05:44:01 +0100

    Setup day 14

---
+++ calendar/day-14/day14.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"advent-of-go-2020/utils/files"
+)
+
+func main() {
+	input := files.ReadFile(14, "\n")
+	println(solvePart1(input))
+}
+
+func solvePart1(input []string) int {
+    result := 0
+
+
+
+    return result
+}
