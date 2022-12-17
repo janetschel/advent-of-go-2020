edd315f16ba5832bfe3cd0ffcdabc5d089875448
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2022-12-17 00:42:25 +0000

    2022 Day 16 pt. 2 started

--- calendar/2022/day-16/day16.go
+++ calendar/2022/day-16/day16.go
@@ -3,9 +3,7 @@ package main
 import (
 	"advent-of-go/utils/files"
 	"advent-of-go/utils/sets"
-	"advent-of-go/utils/slices"
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -14,14 +12,13 @@ type valve struct {
 	name string
 	flowRate int
 	tunnelsTo []string
+	paths map[string]int
 }
 
 type state struct {
 	current string
 	timeRemaining int
-	pressure int
-	open []string
-	path string
+	open map[string]int
 }
 
 func main() {
@@ -32,89 +29,74 @@ func main() {
 
 func solvePart1(input []string) int {
 	valves := parseInput(input)
+	pressures := getMaxPressureReleased(valves, 30, map[string]int{})
+	maxPressure := 0
+	for p := range pressures {
+		if p > maxPressure {
+			maxPressure = p
+		}
+	}
+	return maxPressure
+}
 
-	starting := state{ current: "AA", timeRemaining: 30, pressure: 0, open: []string{} }
-	lookup := map[string]*state{ starting.ToString(): &starting }
-	queue := sets.New()
-	queue.Add(starting.ToString())
+func solvePart2(input []string) int {
+	valves := parseInput(input)
+	pressures := getMaxPressureReleased(valves, 26, map[string]int{})
+	maxPressure := 0
+	for p := range pressures {
+		if p > maxPressure {
+			maxPressure = p
+		}
+	}
+	return maxPressure
+}
 
-	endingPressures := []int{}
+func getMaxPressureReleased(valves map[string]*valve, timeLimit int, open map[string]int) map[int]map[string]int {
+	starting := state{ current: "AA", timeRemaining: timeLimit, open: open }
+	queue := []*state{ &starting }
+
+	endingPressures := map[int]map[string]int{}
 
 	killswitch := 0
-	for queue.Size() > 0 && killswitch < 9999999 {
-		currString := queue.Random()
-		currentState := lookup[currString]
-		currentState.updateTimeAndPressure(valves)
-
-		queue.Remove(currString)
-		lookup[currString] = nil
-
-		lookup[currentState.ToString()] = currentState
+	for len(queue) > 0 && killswitch < 9999999 {
+		currentState := queue[0]
+		queue = queue[1:]
 
 		if currentState.timeRemaining <= 0 {
-			if currentState.pressure > 1600 {
-				fmt.Println(currentState)
-			}
-			endingPressures = append(endingPressures, currentState.pressure)
+			endingPressures[currentState.getEndPressure(valves)] = currentState.open
 		} else {
 			generatedStates := 0
-			// if current has a flow rate and is not open, open in
-			if !slices.Contains(currentState.open, currentState.current) && valves[currentState.current].flowRate > 0 {
-				newState := currentState.copy()
-				newState.open = append(newState.open, currentState.current)
-				lookup[newState.ToString()] = &newState
-				queue.Add(newState.ToString())
-				generatedStates++
-			}
-
-			for _, v := range valves[currentState.current].tunnelsTo {
-				// visit the valves which haven't been opened
-				// if !slices.Contains(currentState.open, v) {
-				visits := strings.Count(currentState.path, v)
-				if visits < len(valves[v].tunnelsTo) {
+			for v, pathLength := range valves[currentState.current].paths {
+				_, open := currentState.open[v]
+				// visit any/all unopened valves
+				if !open && valves[v].flowRate > 0 {
 					newState := currentState.copy()
 					newState.current = v
-					newState.path += v + " "
-					lookup[newState.ToString()] = &newState
-					queue.Add(newState.ToString())
+					if newState.timeRemaining - (pathLength + 1) >= 0 {
+						newState.timeRemaining -= pathLength + 1
+						newState.open[v] = newState.timeRemaining
+						queue = append(queue, &newState)
+					} else {
+						endingPressures[currentState.getEndPressure(valves)] = currentState.open
+					}
 					generatedStates++
 				}
 			}
 
 			// if no moves/opens are relevant, stay in place
 			if generatedStates == 0 {
-				for currentState.timeRemaining > 0 {
-					currentState.updateTimeAndPressure(valves)
-				}
-				if currentState.pressure > 1600 {
-					println("finishing")
-					fmt.Println(currentState)
-				}
-				endingPressures = append(endingPressures, currentState.pressure)
-				// queue.Add(currentState.ToString())
+				endingPressures[currentState.getEndPressure(valves)] = currentState.open
 			}
 		}
 		killswitch++
 	}
 
-	println(killswitch, queue.Size())
-	if len(endingPressures) == 0 {
-		return -1
-	}
-	sort.Ints(endingPressures)
-	return endingPressures[len(endingPressures) - 1]
+	println(killswitch, len(queue), len(endingPressures))
+	return endingPressures
 }
 
-func solvePart2(input []string) int {
-	result := 0
-
-
-
-	return result
-}
-
-func parseInput(input []string) (map[string]valve) {
-	valves := map[string]valve{}
+func parseInput(input []string) (map[string]*valve) {
+	valves := map[string]*valve{}
 	for _, v := range input {
 		parts := strings.FieldsFunc(v, func(r rune) (bool) {
 			return strings.ContainsRune(" =;,", r)
@@ -125,30 +107,57 @@ func parseInput(input []string) (map[string]valve) {
 			flowRate: flowRate,
 			tunnelsTo: parts[10:],
 		}
-		valves[parts[1]] = valve
+		valves[parts[1]] = &valve
+	}
+	
+	for _, v := range valves {
+		v.generateShortestPaths(valves)
 	}
 	return valves
 }
 
-func (s state) ToString() string {
-	return fmt.Sprintf("%s %d %d %v", s.current, s.timeRemaining, s.pressure, s.open)
-}
+func (v *valve) generateShortestPaths(valves map[string]*valve) {
+	queue := []string{v.name}
+	v.paths = map[string]int{}
+	visited := sets.New()
+	visited.Add(v.name)
 
-func (s state) copy() state {
-	newOpen := make([]string, len(s.open))
-	copy(newOpen, s.open)
-	return state{
-		current: s.current,
-		timeRemaining: s.timeRemaining,
-		pressure: s.pressure,
-		open: newOpen,
-		path: s.path,
+	for len(queue) > 0 {
+		current := valves[queue[0]]
+		queue = queue[1:]
+		for _, next := range current.tunnelsTo {
+			if !visited.Has(next) {
+				visited.Add(next)
+				v.paths[next] += v.paths[current.name] + 1
+				queue = append(queue, next)
+			}
+		}
 	}
 }
 
-func (s *state) updateTimeAndPressure(valves map[string]valve) {
-	s.timeRemaining--
-	for _, valve := range s.open {
-		s.pressure += valves[valve].flowRate
+
+
+func (s *state) getEndPressure(valves map[string]*valve) int {
+	pressure := 0
+	for v, t := range s.open {
+		pressure += t * valves[v].flowRate
+	}
+	return pressure
+}
+
+func (s state) ToString() string {
+	return fmt.Sprintf("%s %d %v", s.current, s.timeRemaining, s.open)
+}
+
+func (s state) copy() state {
+	newOpen := map[string]int{}
+	for v, t := range s.open {
+		newOpen[v] = t
+	}
+	return state{
+		current: s.current,
+		timeRemaining: s.timeRemaining,
+		// pressure: s.pressure,
+		open: newOpen,
 	}
 }
